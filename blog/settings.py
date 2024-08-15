bab12ec54115c6b724df76475edc88c8b2d72dd1
BALAxJD <balaxjd@gmail.com>
2024-08-15 17:11:05 +0530

    just try

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j=$cc66&=!)xkl!l)=w*2_xol**u9(%^0mmve((p_e7y(!0f5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = [https://balablog-1ftdayhav-balamurugans-projects-27e50185.vercel.app/']
+ALLOWED_HOSTS = ['https://balablog-1ftdayhav-balamurugans-projects-27e50185.vercel.app/']
 
 
 # Application definition
