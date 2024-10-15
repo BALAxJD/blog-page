6d87c4cd1fe0802e7431391fed37242d5130eded
BALAxJD <balaxjd@gmail.com>
2024-10-15 22:46:34 +0530

    small changes

--- myblog/views.py
+++ myblog/views.py
@@ -5,9 +5,8 @@ def home(request):
 
 def b1(request):
     return(render(request,"blog1.html"))
-<<<<<<< HEAD
+
 
 def b2(request):
     return(render(request,"blog2.html"))
-=======
->>>>>>> fc12c7320fc397f69d72eec92c8cf560ab255993
+
