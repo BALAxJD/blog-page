5ee854746608fa5096de45620c663f9a6591217f
BALAxJD <126006518+BALAxJD@users.noreply.github.com>
2025-10-10 11:44:45 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -70,4 +70,5 @@ urlpatterns = [
     path("How Iceland Lives With Fire and Ice: A Journey Through Volcanoes, Glaciers, and Human Resilience in 2025",views.blog20,name='blog20'),
     path("Top 5 Green Cities in the World That Are Leading the Way Toward Sustainable Urban Living",views.top10,name='top10'),
     path("The Role of Ayurveda and Modern Medicine: Can They Work Together for Better Health in the Modern World?",views.blog21,name='blog21'),
+    path("Mental Health Awareness in the Digital Age: How to Stay Balanced, Focused, and Healthy While Living Online",views.blog22,name='blog22'),
 ]
